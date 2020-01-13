0a97480c8cdf2fa6c818fa75b937bef8dcc14248
Christian Perez <perezchristian1012@gmail.com>
2020-01-13 22:49:03 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,26 @@
 class Post < ActiveRecord::Base
 
-  validate :is_title_case 
-  before_validation :make_title_case 
+  validate :is_title_case
+  before_validation :make_title_case
   belongs_to :author
 
-  #put new code here
+  def index
+  # provide a list of authors to the view for the filter control
+  @authors = Author.all
+
+  # filter the @posts list based on user input
+  if !params[:author].blank?
+    @posts = Post.where(author: params[:author])
+  elsif !params[:date].blank?
+    if params[:date] == "Today"
+      @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+    else
+      @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
+    end
+  else
+    # if no filters are applied, show all posts
+    @posts = Post.all
+  end
 
   private
 
